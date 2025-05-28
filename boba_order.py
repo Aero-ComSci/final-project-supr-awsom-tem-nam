805414108bc44b3150c8b5d9c31a13c4e6760b8b
sr495 <ramasayam0819@mydusd.org>
2025-05-28 11:36:42 -0700

    Update boba_order.py

--- boba_order.py
+++ boba_order.py
@@ -7,7 +7,7 @@ while not amount_of_boba.isdigit() or int(amount_of_boba) < 1:
     amount_of_boba = input("How many boba drinks would you like? ")
 
 boba_ordered = 0
-menu_text = "Boba Menu\n Taro : $7.00\n Thai : $6.50\n Brown Sugar : $7.25\n Mango : $6.75\n Strawberry : $6.75\nToppings ($1.25 each): Honeydew Boba, Crushed Oreos, Lychee Jelly\nComplimentary: Tapioca Pearls"
+menu_text = "Boba Menu\n Taro : $7.00\n Thai : $6.50\n Brown Sugar : $7.25\n Mango : $6.75\n Strawberry : $6.75\n"
 print(menu_text)
 
 while boba_ordered < int(amount_of_boba):
@@ -15,47 +15,37 @@ while boba_ordered < int(amount_of_boba):
     
     if boba_type == "taro":
         print("Adding taro to your order. $7.00 has been added to your total.")
-        order.append("taro \n")
         order_cost += 7.00
         boba_ordered += 1
     elif boba_type == "thai":
         print("Adding thai to your order. $6.50 has been added to your total.")
-        order.append("thai \n")
         order_cost += 6.50
         boba_ordered += 1
     elif boba_type == "brown sugar":
         print("Adding brown sugar to your order. $7.25 has been added to your total.")
-        order.append("brown sugar \n")
         order_cost += 7.25
         boba_ordered += 1
     elif boba_type == "mango":
         print("Adding mango to your order. $6.75 has been added to your total.")
-        order.append("mango \n")
         order_cost += 6.75
         boba_ordered += 1
     elif boba_type == "strawberry":
         print("Adding strawberry to your order. $6.75 has been added to your total.")
-        order.append("strawberry \n")
         order_cost += 6.75
         boba_ordered += 1
     else:
-        print("Invalid input. Please enter a valid option.")
+        boba_type = input("Invalid input. Please enter a valid option.")
+    
+    toppings = input("Would you like any toppings? (Honeydew Boba, Crushed Oreos, Lychee Jelly) Type 'none' if no toppings (complimentary tapioca pearls!): ").lower()
+    if toppings in ["honeydew boba", "crushed oreos", "lychee jelly"]:
+        print("Adding " + toppings + " to your order. $1.25 has been added to your total.")
+        order.append(boba_type + " boba with " + toppings + "\n")
+        order_cost += 1.25
+    elif toppings == "none":
+        print("No extra toppings added.")
+        order.append(boba_type + " boba with tapioca pearls \n")
+    else:
+        toppings = input("Invalid input. Enter a valid topping or 'none'. ")
 
 print("\nYour order:\n" + "".join(order))
 print("Total: $" + str(order_cost))
-
-toppings = input("Would you like any toppings? (Honeydew Boba, Crushed Oreos, Lychee Jelly) Type 'none' if no toppings: ").lower()
-if toppings in ["honeydew boba", "crushed oreos", "lychee jelly"]:
-    print("Adding " + toppings + " to your order. $1.25 has been added to your total.")
-    order.append(toppings + "\n")
-    order_cost += 1.25
-elif toppings == "none":
-    print("No extra toppings added.")
-else:
-    print("Invalid input. No extra toppings added.")
-
-print("Complimentary tapioca pearls have been added to your order!\n")
-
-print("Your order:\n" + "".join(order))
-print("Total: $" + str(order_cost))
-
