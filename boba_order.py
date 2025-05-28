d742dbc5e5e069eba14f8fd5fc9dabb9c7ab4c56
sr495 <ramasayam0819@mydusd.org>
2025-05-28 11:18:08 -0700

    Update boba_order.py

--- boba_order.py
+++ boba_order.py
@@ -7,27 +7,39 @@ while not amount_of_boba.isdigit() or int(amount_of_boba) < 1:
     amount_of_boba = input("How many boba drinks would you like? ")
 
 boba_ordered = 0
-print("Boba Menu \n Taro Milk Tea: $6.50 \n Thai Milk Tea: $6.00 \n Brown Sugar Milk Tea: $6.50 \n Mango Milk Tea: $5.50 \n Strawberry Milk Tea: $5.50")
-print("Toppings ($1.25 each): Honeydew Boba, Crushed Oreos, Lychee Jelly")
-print("Complimentary: Tapioca Pearls")
+menu_text = "Boba Menu\n Taro Milk Tea: $7.00\n Thai Milk Tea: $6.50\n Brown Sugar Milk Tea: $7.25\n Mango Milk Tea: $6.75\n Strawberry Milk Tea: $6.75\nToppings ($1.25 each): Honeydew Boba, Crushed Oreos, Lychee Jelly\nComplimentary: Tapioca Pearls"
+print(menu_text)
 
 while boba_ordered < int(amount_of_boba):
     boba_type = input("Which boba would you like? ").lower()
     
-    if boba_type == "taro milk tea":
-        print("Adding taro milk tea to your order. $6.50 has been added to your total.")
-        order.append(boba_type)
+    if boba_type == "taro":
+        print("Adding taro milk tea to your order. $7.00 has been added to your total.")
+        order.append("taro milk tea\n")
+        order_cost += 7.00
+        boba_ordered += 1
+    elif boba_type == "thai":
+        print("Adding thai milk tea to your order. $6.50 has been added to your total.")
+        order.append("thai milk tea\n")
         order_cost += 6.50
         boba_ordered += 1
-    elif boba_type in ["thai milk tea", "brown sugar milk tea"]:
-        print("Adding " + boba_type + " to your order. $6.00 has been added to your total.")
-        order.append(boba_type)
-        order_cost += 6.00
+    elif boba_type == "brown sugar":
+        print("Adding brown sugar milk tea to your order. $7.25 has been added to your total.")
+        order.append("brown sugar milk tea\n")
+        order_cost += 7.25
         boba_ordered += 1
-    elif boba_type in ["mango milk tea", "strawberry milk tea"]:
-        print("Adding " + boba_type + " to your order. $5.50 has been added to your total.")
-        order.append(boba_type)
-        order_cost += 5.50
+    elif boba_type == "mango":
+        print("Adding mango milk tea to your order. $6.75 has been added to your total.")
+        order.append("mango milk tea\n")
+        order_cost += 6.75
+        boba_ordered += 1
+    elif boba_type == "strawberry":
+        print("Adding strawberry milk tea to your order. $6.75 has been added to your total.")
+        order.append("strawberry milk tea\n")
+        order_cost += 6.75
         boba_ordered += 1
     else:
         print("Invalid input. Please enter a valid option.")
+
+print("\nYour order:\n" + "".join(order))
+print("Total: $" + str(order_cost))
